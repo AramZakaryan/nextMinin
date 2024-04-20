2da5c119cf82be7d9ff4b9ab1778ac92e7eb483f
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-20 08:47:48 +0400

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,19 @@
+import Link from "next/link";
+import Head from "next/head";
 
 export default function Index() {
-    return (
-        <h2>Index</h2>
+    return (<>
+            <Head>
+
+                <title>main page</title>
+                <meta charSet={"utf8"}/>
+                <meta name={"keywords"} content={"next, javascript, react, next"}/>
+                <meta name={"description"} content={"this is first task with next"}/>
+            </Head>
+            <h2>Index</h2>
+            <p><Link href={"/about"}>go to about</Link></p>
+            <p><Link href={"/profile"}>go to profile</Link></p>
+            <p><Link href={"/post/22"}>go to post</Link></p>
+        </>
     );
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,6 +1,9 @@
+import {useRouter} from "next/router";
 
 export default function Profile() {
-    return (
+    const router = useRouter()
+    return (<>
         <h2>Profile</h2>
-    );
+        <button onClick={()=>router.push("/")}>go back to home</button>
+    </>);
 }
